6d8052e4e85e563563bf07f856ff934818b259d6
Jm <jonathan.p.middleton@gmail.com>
2019-03-18 18:24:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const hqBlock=42;
+
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block-hqBlock);
+}
+
+function distanceFromHqInFeet(feet) {
+  return distanceFromHqInBlocks(feet)*264;
+}
+
+let startBlock=
+
+function distanceTravelledInFeet(feet) {
+  return Math.abs(startBlock-endBlock)*264;
+}
+
+
+ //43, 48)).to.equal(1320)//
